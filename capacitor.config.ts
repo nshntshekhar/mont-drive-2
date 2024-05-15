8a95edf7be62f70246491f703c84481e6f95bc5b
nshntshekhar <131431371+nshntshekhar@users.noreply.github.com>
2024-05-15 20:29:53 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.montdrive',
   appName: 'mont-drive-2',
-  webDir: 'www'
+  webDir: 'www',
+  version: '1.0.1'
 };
 
 export default config;
